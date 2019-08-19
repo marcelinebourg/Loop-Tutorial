212686e3e5b3dd64c113d27e51c6e1a6a17b9460
Gabriel Bourg <s198007@students.aie.edu>
2019-08-19 16:06:39 -0500

    Add project files.

---
+++ Loop Tutorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            //new code
+            for( int count = 0; count < 3; count++)
+            {
+                Console.WriteLine("the value of count is now" + count + ".");
+                Console.ReadKey();
+                if (count == 2)
+                {
+                    return;
+                }
+            }
+            Console.WriteLine("the loop is finished");
+            Console.ReadKey();
+            
+                
+            
+        }
+    }
+}
